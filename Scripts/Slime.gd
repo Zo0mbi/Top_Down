cd123980bb83d6400ff02041c23810ea2144b8f9
Zo0mbi <2164459@carrefour.cegepvicto.ca>
2022-11-15 13:10:20 -0500

    Modif de correction

--- Scripts/Slime.gd
+++ Scripts/Slime.gd
@@ -4,13 +4,14 @@ var is_moving = true
 var is_dead = false
 var speed = 25
 onready var obj = get_parent().get_node("Joueur")
-
+onready var _animted_slime = $AnimatedSprite
 
 func _physics_process(delta):
 	deplacer_slime(delta)
 #Reste a implementer le if contact avec joueur se deplacer a linverse.
 func deplacer_slime(delta):
 	deplacer_vers_joueur(delta)
+	_animted_slime.play("movement")
 
 func deplacer_vers_joueur(delta):
 	var dir = (obj.global_position - global_position).normalized()
